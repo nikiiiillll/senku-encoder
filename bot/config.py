37b308a28d8da290b3ce538dda7cc8eeca7eeb9d
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2022-11-05 09:30:45 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,14 +16,14 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1322549723
-    OWNER = config("OWNER")
+    APP_ID = config("APP_ID", "6951121")
+    API_HASH = config("API_HASH", "10654d2aa0d67e2f8759ba6a1644fa65")
+    BOT_TOKEN = config("BOT_TOKEN", "1908889255:AAE3AAwrqkcLcBDncj_Ippeg1Zyf5O0ScQk")
+    DEV = 1482769753
+    OWNER = config("OWNER", "1482769753")
     FFMPEG = config(
         "FFMPEG",
-        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
+        default='ffmpeg -i "{}" -preset superfast -c:v libx265 -crf 28 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
     )
     THUMB = config(
         "THUMBNAIL", default="https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg"
